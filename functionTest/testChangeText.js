0c7a6c72844ed23f3903a281c3fd291c9d9ec9cd
lsohee <limsohee0521@gmail.com>
2022-07-06 20:09:06 +0900

    branch clickEvent issue 1 : 배열 메서드를 이용

--- functionTest/testChangeText.js
+++ functionTest/testChangeText.js
@@ -6,8 +6,18 @@ const testObj = {
   color: "blue"
 }
 
-console.log(testObj.name);
-testArr.unshift(testObj.age)
-console.log(testArr[0]);
-testArr.unshift(testObj.color);
-console.log(testArr[0]);
+// console.log(testObj.name);
+// testArr.unshift(testObj.age)
+// console.log(testArr[0]);
+// testArr.unshift(testObj.color);
+// console.log(testArr[0]);
+
+function changeArr(arrName, pushValue) {
+  arrName.pop()
+  arrName.push(pushValue);
+  return arrName[0]
+}
+
+changeArr(testArr, testObj.name);
+console.log(testArr[0])
+console.log(changeArr(testArr, testObj.age));
