2a9560f71fac8a157b8ad91d761a99b4c2488dde
limsohee <limsohee0521@gmail.com>
2022-07-07 12:57:00 +0900

    #2

--- main.js
+++ main.js
@@ -45,30 +45,55 @@ import pageIFunc from "./pageValues/[02]page-I.js"
 
 
 // pseudo 만들고 있는 인스턴스 값
-//  --> 페이지 Me makeMeHappyText 값 배열
-const makeMeHappyTextArr = [
-  new CreateDivValues("300px", "fit-contant", "absolute", percentCalculator.left(20), percentCalculator.top(20), "10px", "black", "none", "makeMeHappyText", 0),
-  new CreateMovingValues(30, 30, 30, 30, 0.5, 1, 0, 0.3), // pseudo page My (나타남)
-  new CreateMovingValues(30, 45, 30, 30, 1, 2, 0.3, 1), // pseudo page Me (주 컨텐츠)
-  new CreateMovingValues(45, -25, 30, 35, 2, 5, 1, 0.4),
-  new CreateMovingValues(-25, -80, 35, 35, 5, 5.5, 0.4, 0)
+//  --> 페이지 Me meMainText 값 배열
+const meMainTextArr = [
+  new CreateDivValues("300px", "fit-contant", "absolute", percentCalculator.left(20), percentCalculator.top(20), "12px", "black", "none", "meMainText", 0),
+  new CreateMovingValues(30, 20, 30, 30, 0.5, 1, 0, 0.3), // pseudo page My (나타남)
+  new CreateMovingValues(20, 30, 30, 30, 1, 1.5, 0.3, 1), // pseudo page Me (주 컨텐츠)
+  new CreateMovingValues(30, -30, 30, 35, 1.5, 5, 1, 0.4),
+  new CreateMovingValues(-30, -80, 35, 35, 5, 5.5, 0.4, 0)
 ]
+
+
+const fish1Arr = [
+  new CreateDivValues("200px", "fit-contant", "absolute", percentCalculator.left(20), percentCalculator.top(20), "10px", "black", "./img/fish1.svg", "fish1", 0),
+  new CreateMovingValues(100, 70, 30, 30, 0.5, 1, 0, 0.3), // pseudo page My (나타남)
+  new CreateMovingValues(70, 60, 30, 30, 1, 2, 0.3, 1), // pseudo page Me (주 컨텐츠)
+  new CreateMovingValues(60, -25, 30, 35, 2, 5, 1, 1),
+  new CreateMovingValues(-25, -80, 35, 35, 5, 5.5, 1, 0)
+]
+
+
+const fish2Arr = [
+  new CreateDivValues("120px", "fit-contant", "absolute", percentCalculator.left(20), percentCalculator.top(20), "10px", "black", "./img/fish2.svg", "fish2", 0),
+  new CreateMovingValues(100, 80, 30, 70, 0.5, 1, 0, 0.3), // pseudo page My (나타남)
+  new CreateMovingValues(80, 70, 70, 60, 1, 2, 0.3, 1), // pseudo page Me (주 컨텐츠)
+  new CreateMovingValues(70, -25, 60, 35, 2, 5, 1, 1),
+  new CreateMovingValues(-25, -80, 35, 35, 5, 5.5, 1, 0)
+]
+
+
 
 const myself1TextArr = [
   new CreateDivValues("200px", "fit-contant", "absolute", percentCalculator.left(30), percentCalculator.top(20), "10px", "black", "none", "myself1Text", 0),
-  new CreateMovingValues(30, 30, 30, 30, 0.5, 1, 0, 0.3), // pseudo page Me (나타남)
-  new CreateMovingValues(30, 40, 30, 30, 1, 2, 0.3, 1), // pseudo page Meyself1 (주 컨텐츠)
-  new CreateMovingValues(40, -10, 30, 5, 2, 4, 1, 0.4)
+  new CreateMovingValues(30, 30, 30, 30, 0.5, 0.6, 0, 0.2), // pseudo page Me (나타남)
+  new CreateMovingValues(30, 25, 30, 30, 0.6, 1.5, 0.2, 2), // pseudo page Meyself1 (주 컨텐츠)
+  new CreateMovingValues(25, -40, 30, 5, 1.5, 3, 2, 0.4)
 ]
 
 const myself2TextArr = [
   new CreateDivValues("200px", "fit-contant", "absolute", percentCalculator.left(30), percentCalculator.top(20), "10px", "black", "none", "myself2Text", 0),
-  new CreateMovingValues(45, 50, 45, 50, 0.3, 1, 0, 0.3), // pseudo page Meyself1 (나타남)
-  new CreateMovingValues(50, 65, 50, 60, 1, 1.5, 0.3, 1), // pseudo page Meyself1 (나타남)
-  new CreateMovingValues(65, 45, 60, 45, 1.5, 2) // pseudo page Meyself2 (나타남)
+  new CreateMovingValues(45, 50, 45, 50, 0.3, 0.6, 0, 0.3), // pseudo page Meyself1 (나타남)
+  new CreateMovingValues(50, 65, 50, 60, 0.6, 1, 0.3, 1), // pseudo page Meyself1 (나타남)
+  new CreateMovingValues(65, 30, 60, 30, 1, 2) // pseudo page Meyself2 (나타남)
 ]
 
-
+const bottomArr = [
+  new CreateDivValues("1900px", "fit-contant", "absolute", percentCalculator.left(0), percentCalculator.top(0), "10px", "black", "img/대지 1 1.svg", "bottom", 0),
+  new CreateMovingValues(0, 0, 0, 0, 1, 1.1, 0, 0.2), // pseudo page Meyself1 (나타남)
+  new CreateMovingValues(0, 0, 0, 3, 1, 1.2, 0.2, 1), // pseudo page Meyself1 (나타남)
+  new CreateMovingValues(0, -40, 3, -35, 1.2, 2) // pseudo page Meyself2 (나타남)
+]
 
 
 // pseudo JSON파일 통신 , load 됐을 때 작동하는 이벤트 함수 
@@ -82,16 +107,22 @@ window.onload = function () {
 
     //pseudo 내용을 담을 배열 그릇
     const myMainTextArr = [jsonObj.projects]
-    const meMainTextArr = []
+    const meMainTextArrTemp = [jsonObj.happy]
 
     // pseudo 클릭했을 때 배열을 변형하면 되지 않을까?
     // --> 그러면 원형을 건들여야되나?
-
+    
     root2.innerHTML = `
     ${createDiv(jsonObj.intro,attributeNode(introTextArr[0]))}
-    ${createDiv(jsonObj.happy,attributeNode(makeMeHappyTextArr[0]))}
-    ${createDiv(jsonObj.textMyself1,attributeNode(myself1TextArr[0]))}
+    ${createImg("",attributeNodeImg(bottomArr[0]))}
     ${createDiv(jsonObj.textMyself2, attributeNode(myself2TextArr[0]))}
+    ${createDiv(jsonObj.textMyself1,attributeNode(myself1TextArr[0]))}
+
+    
+    ${createImg("",attributeNodeImg(fish1Arr[0]))}
+    ${createImg("",attributeNodeImg(fish2Arr[0]))}
+    ${createDiv(meMainTextArrTemp[0],attributeNode(meMainTextArr[0]))}
+    
     
     ${createImg("",attributeNodeImg(interestsBubbleArr[0]))}
     ${createImg("",attributeNodeImg(projectsBubbleArr[0]))}
@@ -116,7 +147,11 @@ window.onload = function () {
     const interestsBubble = document.getElementById("interestsBubble")
 
 
-    const makeMeHappyText = document.getElementById("makeMeHappyText")
+    const meMainText = document.getElementById("meMainText")
+    const fish1 = document.getElementById("fish1")
+    const fish2 = document.getElementById("fish2")
+    
+    const bottom = document.getElementById("bottom")
     const myself1Text = document.getElementById("myself1Text")
     const myself2Text = document.getElementById("myself2Text")
 
@@ -127,6 +162,10 @@ window.onload = function () {
     prosAndConsBubble.addEventListener("click",()=>{changeArr(myMainTextArr, jsonObj.prosAndCons,mainBubbleText)})
     interestsBubble.addEventListener("click",()=>{changeArr(myMainTextArr, jsonObj.interests,mainBubbleText)})
 
+
+    // pseudo fishes click event
+    fish1.addEventListener("click",()=>{changeArr(meMainTextArrTemp, jsonObj.happy,meMainText)})
+    fish2.addEventListener("click",()=>{changeArr(meMainTextArrTemp, jsonObj.need,meMainText)})
     
     
     let currentPage = [true, false, false, false, false, false]
@@ -163,7 +202,9 @@ window.onload = function () {
 
 
           // --> page Me elements
-          zoom(makeMeHappyText, "normal", makeMeHappyTextArr[1])
+          zoom(meMainText, "normal", meMainTextArr[1])
+          zoom(fish1,"normal",fish1Arr[1])
+          zoom(fish2,"normal",fish2Arr[1])
 
 
         } else if (event.wheelDelta < 0 && currentPage[2] === true) {
@@ -182,11 +223,14 @@ window.onload = function () {
 
 
           // --> page Me elements
-          zoom(makeMeHappyText, "normal", makeMeHappyTextArr[2])
+          zoom(meMainText, "normal", meMainTextArr[2])
+          zoom(fish1,"normal",fish1Arr[2])
+          zoom(fish2,"normal",fish2Arr[2])
 
 
           // --> page Myself elements
           zoom(myself1Text, "normal", myself1TextArr[1])
+          zoom(bottom, "normal", bottomArr[1])
           zoom(myself2Text, "normal", myself2TextArr[1])
 
 
@@ -203,7 +247,13 @@ window.onload = function () {
 
 
 
-          zoom(makeMeHappyText, "normal", makeMeHappyTextArr[3])
+          zoom(meMainText, "normal", meMainTextArr[3])
+          zoom(fish1,"normal",fish1Arr[3])
+          zoom(fish2,"normal",fish2Arr[3])
+          
+          
+          
+          zoom(bottom, "normal", bottomArr[2])
           zoom(myself1Text, "normal", myself1TextArr[2])
           zoom(myself2Text, "normal", myself2TextArr[2])
 
@@ -218,10 +268,12 @@ window.onload = function () {
           console.log("Myself1에서 Myself2로 이동합니다")
 
 
+          zoom(meMainText, "normal", meMainTextArr[4])
+          zoom(fish1,"normal",fish1Arr[4])
 
           zoom(myself1Text, "normal", myself1TextArr[3])
-          zoom(makeMeHappyText, "normal", makeMeHappyTextArr[4])
           zoom(myself2Text, "normal", myself2TextArr[3])
+          zoom(bottom, "normal", bottomArr[3])
 
 
 
@@ -238,7 +290,7 @@ window.onload = function () {
           console.log("Myself2에서 Myself1로 이동합니다")
           currentPage.splice(4, 2, true, false)
           zoom(myself1Text, "reverse", myself1TextArr[3])
-          zoom(makeMeHappyText, "reverse", makeMeHappyTextArr[4])
+          zoom(meMainText, "reverse", meMainTextArr[4])
           zoom(myself2Text, "reverse", myself2TextArr[3])
 
 
@@ -256,7 +308,7 @@ window.onload = function () {
           zoom(mottoBubble, "reverse", mottoBubbleArr[4])
 
 
-          zoom(makeMeHappyText, "reverse", makeMeHappyTextArr[3])
+          zoom(meMainText, "reverse", meMainTextArr[3])
           zoom(myself1Text, "reverse", myself1TextArr[2])
           zoom(myself2Text, "reverse", myself2TextArr[2])
 
@@ -275,7 +327,7 @@ window.onload = function () {
 
 
 
-          zoom(makeMeHappyText, "reverse", makeMeHappyTextArr[2])
+          zoom(meMainText, "reverse", meMainTextArr[2])
           zoom(myself1Text, "reverse", myself1TextArr[1])
           zoom(myself2Text, "reverse", myself2TextArr[1])
 
@@ -301,7 +353,7 @@ window.onload = function () {
           zoom(interestsBubble, "reverse", interestsBubbleArr[2])
 
           // --> page Me elements
-          zoom(makeMeHappyText, "reverse", makeMeHappyTextArr[1])
+          zoom(meMainText, "reverse", meMainTextArr[1])
 
 
 
